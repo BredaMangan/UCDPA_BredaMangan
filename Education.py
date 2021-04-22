8b5b20921b56ca431bb23c84edcf650d276f6191
Breda Mangan <breda.mangan@hse.ie>
2021-04-22 15:31:57 +0100

    "Fourth Commit"

--- Education.py
+++ Education.py
@@ -61,27 +61,50 @@ expenditure = int(42587272 + 26058021 + 3995951 + 6786142)
 total_revenue = 89217262
 total_expenditure = 85320133
 
+## Check if the expenditure is equal to the total expenditure
 if total_expenditure == expenditure:
     print("total expenditure is equal to expenditure")
 elif total_expenditure < expenditure:
     print("total expenditure is less than expenditure")
 else:print("total expenditure is greater than ependiture")
 
+##check if the revenue is equal to the total revenue
 if total_revenue == revenue:
     print("total revenue is equal to total revenue")
 elif total_revenue < revenue:
     print("total revenue is less than revenue")
 else:print("total revenue is greater than revenue")
 
+## Create a list of the Columns with revenue and expenditure.
+column_list = ['TOTAL_REVENUE', 'FEDERAL_REVENUE', 'STATE_REVENUE', 'LOCAL_REVENUE', 'TOTAL_EXPENDITURE', 'INSTRUCTION_EXPENDITURE', 'SUPPORT_SERVICES_EXPENDITURE', 'OTHER_EXPENDITURE', 'CAPITAL_OUTLAY_EXPENDITURE']
+print(column_list)
 
+## Divide the columns in the column_list by 1000000
+states_finance[column_list] = states_finance[column_list].div(100000).round(2)
+print(states_finance.shape)
 
+## Create list of columns that show breakdown of Revenue and Expenditure
+column_list_drop = ['FEDERAL_REVENUE', 'STATE_REVENUE', 'LOCAL_REVENUE', 'INSTRUCTION_EXPENDITURE', 'SUPPORT_SERVICES_EXPENDITURE', 'OTHER_EXPENDITURE', 'CAPITAL_OUTLAY_EXPENDITURE']
+print(column_list_drop)
 
+## Drop list of columns that show breakdown of Revenue and Expenditure
+column_list_drop = states_finance.drop(column_list_drop, axis = 1)
+states_finance = column_list_drop
+print(states_finance)
 
-
-
-
-
-
+## Create a list of the States using an array
+array = np.array(['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+       'Colorado', 'Connecticut', 'Delaware', 'District of Columbia',
+       'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana',
+       'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
+       'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi',
+       'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+       'New Jersey', 'New Mexico', 'New York', 'North Carolina',
+       'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania',
+       'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee',
+       'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington',
+       'West Virginia', 'Wisconsin', 'Wyoming'])
+print(array)
 
 
 
